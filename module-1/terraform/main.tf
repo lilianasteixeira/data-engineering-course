c3db31eb854e0b57d77c894f76893c635a3499fa
lilianasteixeira <liliana_s_teixeira@hotmail.com>
2026-01-20 17:56:57 +0000

    terraform

--- module-1/terraform/main.tf
+++ module-1/terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "7.16.0"
     }
   }
@@ -10,5 +10,20 @@ terraform {
 provider "google" {
   credentials = file("/Users/lilianateixeira/Downloads/my-creds.json")
   project     = "peppy-caster-484310-c1"
-  region      = "us-central1"
+  region      = "europe-central2"
+}
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "peppy-caster-484310-c1-terra-bucket"
+  location      = "EUROPE-CENTRAL2"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
